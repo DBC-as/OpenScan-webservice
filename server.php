df9300dd11490a0fae6c8dbaa08cefb77bb5f2f3
pjo <pjo@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2010-02-23 08:51:46 +0000

    With HowRU and ShowInfo
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/OpenScan/trunk@8066 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- server.php
+++ server.php
@@ -5,16 +5,36 @@ require_once("OLS_class_lib/webServiceServer_class.php");
 require_once("OLS_class_lib/curl_class.php");
 // required for handling xml
 require_once("OLS_class_lib/xml_func_class.php");
+// required for caching
+require_once("OLS_class_lib/cache_client_class.php");
 
 
 class openscan_server extends webServiceServer 
 {
+  private static $xsd=null;
+
+  public function __construct($inifile,$schema=null)
+  {
+    //    cache::flush();
+    if( $schema )
+      {
+	if( self::$xsd==null )
+	  {
+	    $dom=new DOMDocument();
+	    $dom->load($schema);
+	    self::$xsd=new DOMXPath($dom);
+	  }
+      }
+    parent::__construct($inifile);    
+  }
  
   public function openScan($params)
   {
-    $terms = methods::openscan_request($params,$this->config,$this->verbose);
- 
-
+    //if( !$terms= $this->terms($params) )
+    //  $terms = methods::openscan_request($params,$this->config,$this->verbose);
+    
+    $terms=$this->terms($params);
+   
     foreach( $terms as $term )
       {
         $response_xmlobj->scanResponse->_namespace="http://oss.dbc.dk/ns/openscan";
@@ -23,21 +43,86 @@ class openscan_server extends webServiceServer
 
     return $response_xmlobj;
   }
+
+   /** \brief Echos config-settings
+   *
+   */
+  public function show_info() 
+  {
+    echo "<pre>";
+    echo "version             " . $this->config->get_value("version", "setup") . "<br/>";
+    echo "log                 " . $this->config->get_value("logfile", "setup") . "<br/>";
+    echo "</pre>";
+    die();
+  }
+
+  public static function fields()
+  {
+    $query="//xs:simpleType[@name='fieldType']/xs:restriction/xs:enumeration";
+
+     if( $data=cache::get($query) )
+       {
+	 return $data;
+       }
+
+    $nodelist=self::$xsd->query($query);
+    //    echo $nodelist->length;
+    $fields=array();
+    foreach( $nodelist as $node )
+      $fields[]=$node->getAttribute("value");
+    
+    cache::set($query,$fields);
+    
+    return $fields;
+  }
+
+  private function terms($params)
+  {
+    $key=methods::cache_key($params);
+    $log=new cache_log("openscan");	
+    if( $data=cache::get($key) )
+      {
+	$log->hit();
+	return $data;
+      }
+    $log->miss();
+
+    $data = methods::openscan_request($params,$this->config,$this->verbose);
+
+    cache::set($key,$data);
+    return $data;    
+  }
 }
 
-$server=new openscan_server("openscan.ini");
+$server=new openscan_server("openscan.ini","openscan.xsd");
 $server->handle_request();
 
 class methods
 {
+
+  /**
+     @make a cache_key
+   */
+  public static function cache_key($params)
+  {
+    foreach($params as $key=>$value)
+      $cachekey.=$key.$value->_value;
+
+    return $cachekey;
+  }
+
  /** Function for handling scan-request 
    *  @param params; The request mapped to params-object
    *  @return response;an array of terms, false if something went wrong
    */
   public static function openscan_request($params,$config=null,$verbose=null)
   {
+    $fields=openscan_server::fields();
+    //    print_r($hest);
+    //exit;
+
     // make an url for request
-    if( !$query=self::get_query($params) )
+    if( !$query=self::get_query($params,$fields) )
       {
 	if( $verbose )
 	  $verbose->log(WARNING,"openScan:224::could not set query for solr");
@@ -113,18 +198,35 @@ class methods
    *  @param params; params-object
    *  @return ret; given params mapped to url-parameters
    */
-  private static function get_query($params)
+  private static function get_query($params,$fields=null)
   {
     $prefix="&terms.";
     // field and limit are the only required values 
-    if( ! $params->field->_value || ! $params->limit->_value )
+    if( ! $field=$params->field->_value || ! $params->limit->_value )
       return false;
+
+    $field=$params->field->_value;
+
+    // field check
+    if( $fields )
+      {
+	$flag=false;
+	foreach($fields as $key=>$val)
+	  if( $val==$field )
+	    {
+	      $flag=true;
+	      break;
+	    }      
+	if( !$flag )
+	  die( "error in request; field not valid" );
+      }
     
     $ret.= $prefix."fl=".$params->field->_value;
     $ret.= $prefix."rows=".$params->limit->_value;
     
-    if( $params->lower->_value )
-      $ret.= $prefix."lower=".$params->lower->_value;
+    if( $lower=$params->lower->_value )
+      $ret.= $prefix."lower=".strtolower($lower);
+
     
     if( $params->minFrequency->_value )
       $ret.= $prefix."mincount=".$params->minFrequency->_value;
@@ -138,6 +240,9 @@ class methods
     if( $params->upper->_value )
       $ret.= $prefix."upper=".$params->upper->_value;
     
+    //always sort by index
+    $ret.=$prefix."sort=index";  
+
     return $ret;          
   }
 
